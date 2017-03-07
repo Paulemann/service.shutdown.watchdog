fff0c271cce2f35c33a867ece0347f5a8ca0d87b
Paulemann <markra@hotmail.de>
2017-03-07 22:47:45 +0100

    Update check_idle.py

--- check_idle.py
+++ check_idle.py
@@ -115,7 +115,7 @@ def load_settings():
     global watched_local, watched_remote, watched_procs, pvr_local, pvr_port, pvr_minsecs, busy_notification
 
     # Defaults:
-    busy_notification = 'Notification(' + __localize__('30008') + ')'
+    busy_notification = 'Notification(' + __localize__(30008) + ')'
 
     try:
         pvr_minsecs = int(float(__setting__('pvrwaketime')) * 60)
