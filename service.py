30fc86354a519c4e00d52ce35a3bd3aa3f8d6a09
Paulemann <markra@hotmail.de>
2017-03-07 21:04:09 +0100

    Update service.py

--- service.py
+++ service.py
@@ -17,16 +17,17 @@ class MyMonitor( xbmc.Monitor ):
         xbmc.Monitor.__init__( self )
 
     def onSettingsChanged( self ):
-        load_settings()
+        load_addon_settings()
 
 
-def load_settings():
+def load_addon_settings():
     try:
         sleep_time = int(__setting__('sleep'))
     except ValueError:
         sleep_time = 60
 
     xbmc.log(msg='[{}] Settings loaded.'.format(__addon_id__), level=xbmc.LOGNOTICE)
+
     return
 
 
@@ -34,7 +35,7 @@ if __name__ == '__main__':
     monitor = MyMonitor()
     xbmc.log(msg='[{}] Addon started.'.format(__addon_id__), level=xbmc.LOGNOTICE)
 
-    load_settings()
+    load_addon_settings()
 
     while not monitor.abortRequested():
         #cmd = 'RunScript(' + __addon_id__ + '\'InhibitIdleShutdown(true)\', \'InhibitIdleShutdown(false)\')'
