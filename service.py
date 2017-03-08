fce48526b42dc9b3e8c3e22c9a62a00854bf1b6c
Paulemann <markra@hotmail.de>
2017-03-08 19:04:07 +0100

    Update service.py

--- service.py
+++ service.py
@@ -4,14 +4,11 @@
 import xbmc
 import xbmcaddon
 
-xbmc.log(msg='[{}] Addon started.'.format(__addon_id__), level=xbmc.LOGNOTICE)
-
 import check_idle, load_addon_settings, sleep_time
 
+
 __addon__ = xbmcaddon.Addon()
-__setting__ = __addon__.getSetting
 __addon_id__ = __addon__.getAddonInfo('id')
-__localize__ = __addon__.getLocalizedString
 
 
 class MyMonitor( xbmc.Monitor ):
@@ -26,6 +23,6 @@ if __name__ == '__main__':
     monitor = MyMonitor()
 
     while not monitor.abortRequested():
-        check_idle('InhibitIdleShutdown(true)', 'InhibitIdleShutdown(false)')
+        check_idle('InhibitIdleShutdown(false)', 'InhibitIdleShutdown(true)')
         if monitor.waitForAbort(float(sleep_time())):
             break
