7ec9677299c3f2fccbb98f57b1edd6f8c731a888
Paulemann <markra@hotmail.de>
2018-09-21 16:35:20 +0200

    code optimizations

--- check_idle.py
+++ check_idle.py
@@ -7,9 +7,12 @@
 import xbmc
 import xbmcaddon
 
+#import psutil
 import subprocess
 import os
+
 import time
+import calendar
 import _strptime
 import sys
 import getopt
@@ -26,6 +29,8 @@ __setting__ = __addon__.getSetting
 __addon_id__ = __addon__.getAddonInfo('id')
 __localize__ = __addon__.getLocalizedString
 
+kodi = 'kodi'
+#kodi = 'kodi-standalone'
 
 #
 # Source: http://stackoverflow.com/questions/10009753/python-dealing-with-mixed-encoding-files
@@ -60,23 +65,20 @@ def get_opts():
     if len(args) == 1 and not idle_action:
         idle_action = args[0]
 
-    if idle_action and ((idle_action[0] == '\'' and idle_action[-1] == '\'') or (idle_action[0] == '\"' and idle_action[-1] == '\"')):
-        idle_action = idle_action[1:-1]
+    if idle_action:
+        idle_action = idle_action.strip('\'\"')
 
-    if busy_action and ((busy_action[0] == '\'' and busy_action[-1] == '\'') or (busy_action[0] == '\"' and busy_action[-1] == '\"')):
-        busy_action = busy_action[1:-1]
+    if busy_action:
+        busy_action = busy_action.strip('\'\"')
 
     return idle_action, busy_action
 
 
 def kodi_is_running():
-    try:
-        pid = subprocess.check_output(['pgrep', 'kodi-standalone'])
+    if active_proc(kodi):
+        return True
 
-    except subprocess.CalledProcessError:
-        return False
-
-    return True
+    return False
 
 
 def is_number(s):
@@ -103,9 +105,9 @@ def port_trans(plist):
     return ret
 
 
-def read_set(string):
+def read_set(item, default):
     ret = set()
-    for element in string.split(','):
+    for element in read_val(item, default).split(','):
         try:
             item = int(element)
         except ValueError:
@@ -114,47 +116,36 @@ def read_set(string):
     return ret
 
 
+def read_val(item, default):
+    try:
+        value = int(__setting__(item))
+    except ValueError:
+        try:
+            if __setting__(item).lower() == 'true' or __setting__(item).lower() == 'false':
+                value = bool(__setting__(item).lower() == 'true')
+            else:
+                value = __setting__(item)
+        except ValueError:
+            value = default
+
+    return value
+
+
 def load_addon_settings():
     global sleep_time, watched_local, watched_remote, watched_procs, pvr_local, pvr_port, pvr_minsecs, busy_notification
 
     busy_notification = 'Notification({})'.format(__localize__(30008).encode('utf-8'))
 
-    try:
-        sleep_time = int(__setting__('sleep'))
-    except ValueError:
-        sleep_time = 60  # 1 min.
+    sleep_time     = read_val('sleep', 60)                    # 60 secs.
+    pvr_minsecs    = read_val('pvrwaketime', 5) * 60          # 5 mins.
+    pvr_port       = read_val('pvrport', 34890)               # VDR-VNSI
+    pvr_local      = read_val('pvrlocal', True)               # PVR backend on local system
 
-    try:
-        pvr_minsecs = int(float(__setting__('pvrwaketime')) * 60)
-    except ValueError:
-        pvr_minsecs = 300 # 5 mins.
+    watched_local  = read_set('localports', '445, 2049')      # smb, nfs, or 'set()' for empty set
+    watched_remote = read_set('remoteports', '22, 445')       # ssh, smb
+    watched_procs  = read_set('procs', 'HandBrakeCLI, ffmpeg, makemkv, makemkvcon')
 
-    try:
-        pvr_port = int(__setting__('pvrport'))
-    except ValueErrror:
-        pvr_port = 34890  # VDR-VNSI
-
-    try:
-        pvr_local = True if __setting__('pvrlocal').lower() == 'true' else False
-    except:
-        pvr_local = True  # PVR backend on local system
-
-    try:
-        watched_local = read_set(__setting__('localports'))
-    except:
-        watched_local = {445, 2049}      #smb, nfs, or 'set()' for empty set
-
-    try:
-        watched_remote = read_set(__setting__('remoteports'))
-    except:
-        watched_remote = {22, 445}       #ssh, smb
-
-    try:
-        watched_procs = read_set(__setting__('procs'))
-    except:
-        watched_procs = {'HandBrakeCLI', 'ffmpeg', 'makemkv' , 'makemkvcon'}
-
-    watched_local = port_trans(watched_local)
+    watched_local  = port_trans(watched_local)
     watched_remote = port_trans(watched_remote)
 
     if __name__ != '__main__':
@@ -167,7 +158,7 @@ def get_sleep_time():
     return sleep_time
 
 
-def jsonrpc_request(method, params=None, host='localhost', port=8080):
+def jsonrpc_request(method, params=None, host='localhost', port=8080, username=None, password=None):
     # e.g. KodiJRPC_Get("PVR.GetProperties", {"properties": ["recording"]})
 
     url = 'http://{}:{}/jsonrpc'.format(host, port)
@@ -180,6 +171,10 @@ def jsonrpc_request(method, params=None, host='localhost', port=8080):
 
     if params:
         jsondata['params'] = params
+
+    if username and password:
+        base64str = base64.encodestring('{}:{}'.format(username, password))[:-1]
+        header['Authorization'] = 'Basic {}'.format(base64str)
 
     try:
         if host == 'localhost':
@@ -203,34 +198,16 @@ def jsonrpc_request(method, params=None, host='localhost', port=8080):
 
 
 def find_clients(port, include_localhost):
-    #clients = set()
-    clients = []
+    clients = set()
 
-    my_env = os.environ.copy()
-    my_env['LC_ALL'] = 'en_EN'
-    netstat = subprocess.check_output(['netstat', '-t', '-n'], universal_newlines=True, env=my_env)
-
-    for line in netstat.split('\n')[2:]:
-        items = line.split()
-        if len(items) < 6 or (items[5] != 'ESTABLISHED'):
+    for conn in active_conns():
+        #if conn.status != psutil.CONN_ESTABLISHED or not conn.raddr:
+        if conn.status != 'ESTABLISHED' or not conn.raddr:
             continue
 
-        local_addr, local_port = items[3].rsplit(':', 1)
-        remote_addr, remote_port = items[4].rsplit(':', 1)
-
-        if local_addr[0] == '[' and local_addr[-1] == ']':
-            local_addr = local_addr[1:-1]
-
-        if remote_addr[0] == '[' and remote_addr[-1] == ']':
-            remote_addr = remote_addr[1:-1]
-
-        local_port = int(local_port)
-
-        if local_port == port:
-            if remote_addr not in clients:
-                if remote_addr != local_addr or include_localhost:
-                    #clients.add(remote_addr) # doesn't require "if remote_addr not in clients:"
-                    clients.append(remote_addr)
+        if int(conn.laddr[1]) == port:
+            if conn.raddr[0] != conn.laddr[0] or include_localhost:
+                clients.add(conn.raddr[0])
 
     return clients
 
@@ -275,10 +252,10 @@ def check_timers():
     try:
         if data:
             timers = data['timers']
-            for i in range(0, len(timers)-1):
-                starttime = int(time.mktime(time.strptime(timers[i]['starttime'], '%Y-%m-%d %H:%M:%S')))
-                endtime = int(time.mktime(time.strptime(timers[i]['endtime'], '%Y-%m-%d %H:%M:%S')))
-                now = int(time.mktime(time.gmtime()))
+            for i in range(len(timers)):
+                starttime = int(calendar.timegm(time.strptime(timers[i]['starttime'], '%Y-%m-%d %H:%M:%S')))
+                endtime = int(calendar.timegm(time.strptime(timers[i]['endtime'], '%Y-%m-%d %H:%M:%S')))
+                now = int(calendar.timegm(time.gmtime()))
 
                 if starttime <= 0 or endtime <= now:
                     continue
@@ -303,53 +280,67 @@ def check_timers():
     return False
 
 
+#def active_conns():
+#    return psutil.net_connections(kind='tcp4')
+
+
+from collections import namedtuple
+
+def active_conns():
+    my_env = os.environ.copy()
+    my_env['LC_ALL'] = 'en_EN'
+    netstat = subprocess.check_output(['netstat', '-tn'], universal_newlines=True, env=my_env)
+
+    connections = []
+    connection = namedtuple('connection', 'laddr raddr status')
+    for line in netstat.split('\n')[2:]:
+        if len(line) > 5:
+            conn = connection(line.split()[3].rsplit(':', 1), line.split()[4].rsplit(':', 1), line.split()[5])
+            connections.append(conn)
+
+    return connections
+
+
+#def active_proc(list):
+#    for proc in psutil.process_iter(attrs=['pid', 'name']):
+#        if proc.status() != psutil.STATUS_ZOMBIE and proc.info['name'].lower() in list:
+#            return proc.info['name']
+#
+#    return None
+
+
+def active_proc(list):
+    response = subprocess.check_output(['ps', 'cax'], universal_newlines=True)
+    procs = [line.split()[-1].lower() for line in response.split('\n')[1:] if line]
+    for proc in procs:
+       if proc in list:
+           return proc
+
+    return None
+
+
 def check_procs():
-    procs = subprocess.check_output(['ps', 'cax'], universal_newlines=True)
+    plist = [element.lower() for element in watched_procs]
 
-    lines = procs.split('\n')[1:]
-
-    for line in lines:
-        items = line.split()
-        if len(items) < 5:
-            continue
-
-        proc = items[4];
-        #if proc in watched_procs:
-        if proc.lower() in [element.lower() for element in watched_procs]:
-            if __name__ == '__main__':
-                xbmc_log('Found active process of {}.'.format(proc))
-            return True
+    pname = active_proc(plist)
+    if pname:
+        if __name__ == '__main__':
+            xbmc_log('Found active process of {}.'.format(pname))
+        return True
 
     return False
 
 
 def check_services():
-    my_env = os.environ.copy()
-    my_env['LC_ALL'] = 'en_EN'
-    netstat = subprocess.check_output(['netstat', '-t', '-n'], universal_newlines=True, env=my_env)
-
-    lines = netstat.split('\n')[2:]
-
-    for line in lines:
-        items = line.split()
-        if len(items) < 6 or (items[5] != 'ESTABLISHED'):
+    for conn in active_conns():
+        #if conn.status != psutil.CONN_ESTABLISHED or not conn.raddr:
+        if conn.status != 'ESTABLISHED' or not conn.raddr:
             continue
 
-        local_addr, local_port = items[3].rsplit(':', 1)
-        remote_addr, remote_port = items[4].rsplit(':', 1)
-
-        if local_addr[0] == '[' and local_addr[-1] == ']':
-            local_addr = local_addr[1:-1]
-
-        if remote_addr[0] == '[' and remote_addr[-1] == ']':
-            remote_addr = remote_addr[1:-1]
-
-        local_port = int(local_port)
-
-        if ((local_addr != remote_addr) and (local_port in watched_remote)) or \
-            ((local_addr == remote_addr) and (local_port in watched_local)):
+        if ((conn.laddr[0] != conn.raddr[0]) and (int(conn.laddr[1]) in watched_remote)) or \
+           ((conn.laddr[0] == conn.raddr[0]) and (int(conn.laddr[1]) in watched_local)):
             if __name__ == '__main__':
-                xbmc_log('Found active connection on port {}.'.format(local_port))
+                xbmc_log('Found active connection on port {}.'.format(conn.laddr[1]))
             return True
 
     return False
